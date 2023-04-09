fb5ad8bc936e8d5573255d04310939be4ec03e9f
Chuang Yu <cyu9960@gmail.com>
2023-04-09 23:05:48 +0800

    Support default prompt

--- bin/ai.mjs
+++ bin/ai.mjs
@@ -13,19 +13,21 @@ async function main() {
     { name: "help", alias: "h", type: Boolean },
     { name: "message", alias: "m", type: String, defaultOption: true },
     { name: "interactive", alias: "i", type: Boolean },
+    { name: "prompt", alias: "p", type: String },
   ];
   const options = commandLineArgs(optionDefinitions);
 
   if (options.help || (!options.message && !options.interactive)) {
     log("Usage: ai [options] [message]");
     log("Options:");
-    log("  -h, --help     Show this help message.");
-    log("  -m, --message  Message to send to the assistant.");
+    log("  -h, --help         Show this help message.");
+    log("  -m, --message      Message to send to the assistant.");
     log("  -i, --interactive  Start an interactive chat session.");
+    log("  -p, --prompt       Prompt to use for the assistant.");
     return;
   }
 
-  const chat = new OpenAIChat();
+  const chat = new OpenAIChat(options.prompt);
 
   // Interactive mode
   if (options.interactive) {
