7ac0d6a3d3279e3aa04975d640c84c6b65ca8094
Chuang Yu <cyu9960@gmail.com>
2023-04-12 22:49:35 +0800

    Add basic tests

---
+++ test.mjs
@@ -0,0 +1,24 @@
+import test from "ava";
+import process from "node:process";
+import { OpenAIChat } from "./lib/OpenAIChat.mjs";
+
+test.beforeEach((t) => {
+  process.env.OPENAI_API_BASE = "1";
+  process.env.OPENAI_API_KEY = "2";
+  process.env.OPENAI_API_MODEL_NAME = "3";
+});
+
+test("api key and url are set correctly", (t) => {
+  const chat = new OpenAIChat();
+  t.is(chat._apiKey, "2");
+  t.is(
+    chat._url,
+    `1/openai/deployments/3/chat/completions?api-version=2023-03-15-preview`
+  );
+});
+
+test("initial prompt is set correctly", (t) => {
+  console.log();
+  const chat = new OpenAIChat("hello");
+  t.is(chat._initialContext.content, "hello");
+});
